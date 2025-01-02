b2cf215eded4dc1387013729f6036917c15237c1
tseloteyonas <nomiyonas21@gmail.com>
2025-01-02 12:53:08 -0800

    route defined

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get("/",(req,res) => {
+    res.send("server is ready");
+});
+
 app.listen(5000 , () => {
     console.log ('Server started at http://localhost:5000');
 });
