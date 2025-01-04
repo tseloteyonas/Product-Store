5f1d67e284c05a89c754f5a2d14394f744d57feb
tseloteyonas <nomiyonas21@gmail.com>
2025-01-03 18:44:52 -0800

    adding MongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-app.get("/",(req,res) => {
-    res.send("server is ready");
+app.get("/products",(req,res) => {
+    
 });
+
+console.log(process.env.MONGO_URI);
 
 app.listen(5000 , () => {
     console.log ('Server started at http://localhost:5000');
 });
+
